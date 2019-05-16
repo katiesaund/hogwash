4648331aa7a27b41a1bd945537e5a228c9ed616d
katiembrennan <ktbrennan3@gmail.com>
2019-05-16 16:28:06 -0400

    added a rm ls() line to demo_data.R because a .Rdata file saving the workspace got saved one time and it overwrites one of the functions. I need to figure out how to delete the Rdata file so I don't have to resort to clearing the workspace with the rm command everytime I open the project.

--- microbeGWAS/demo_data.R
+++ microbeGWAS/demo_data.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library(microbeGWAS)
 library(ape)       # ape::ace function (ancestral reconstruction)
 library(phytools)  # phylogenetic tree function library
@@ -7,6 +8,7 @@ library(phangorn)
 library(pheatmap) # plots for continuous phenotypesq
 library(ggplot2) # plots for continuous phenotypes
 library(geiger)
+library(testthat)
 
 test_dir <- "/nfs/esnitkin/Project_Cdiff/Analysis/Hanna_paper/2019-05-08_gwas_temp_data/data"
 # test_dir   <- "/nfs/esnitkin/Project_Cdiff/Analysis/Hanna_paper/2019-03-18_gwas_fix_geno_to_be_trans/data/"
@@ -14,8 +16,8 @@ test_dir <- "/nfs/esnitkin/Project_Cdiff/Analysis/Hanna_paper/2019-05-08_gwas_te
 # dataset <- 1 # Discrete, FQR, gene test not built from SNPs
 # dataset <- 2 # Continuous, toxin, gene test built from SNPS
 # dataset <- 3 # Continuous, toxin, gene test not built from SNPs
-dataset <- 4 # Continuous, toxin, STOP SNP
-#dataset <- 5 # discrete, severity, stop snp
+# dataset <- 4 # Continuous, toxin, STOP SNP
+dataset <- 5 # discrete, severity, stop snp
 
 if (dataset == 0){
   # Discrete, gene test built from SNPS
@@ -104,8 +106,13 @@ args$perm                   <- as.numeric(test_perm) #typically 10,000
 args$fdr                    <- as.numeric(test_fdr)
 args$annotation             <- NULL #read_in_tsv_matrix(test_annot)
 args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr, args$annot)
-args$bootstrap_cutoff       <- test_bootstrap
+args$bootstrap_cutoff       <- as.numeric(test_bootstrap)
 args$gene_snp_lookup        <- read_in_tsv_matrix(test_gene_snp_lookup)
 args$built_from_snps        <- test_built_from_snps
+
+
+if (dataset == 5){
+  args$genotype <- args$genotype[ , 1:500]
+}
 
 run_phyc(args)
