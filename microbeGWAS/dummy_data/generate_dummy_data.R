827a62a111c8b34b7ed9d66e74821966c72c8551
katiembrennan <ktbrennan3@gmail.com>
2019-05-22 20:22:32 -0400

    added a directory and script to generate and store test data for the test_run_function.R unit tests

---
+++ microbeGWAS/dummy_data/generate_dummy_data.R
@@ -0,0 +1,31 @@
+library(ape)
+set.seed(1)
+tree <- rtree(7)
+tree$edge.length <- rep(sum(tree$edge.length)/Nedge(tree), Nedge(tree))
+tree$node.label <- c(100, 100, 50, 100, 100, 100) # 1 low confidence edge
+
+discrete_phenotype <- matrix(c(0, 1, 1, 1, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+row.names(discrete_phenotype) <- tree$tip.label
+colnames(discrete_phenotype) <- "resistance"
+
+genotype1 <- matrix(c(0, 1, 0, 1, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype2 <- matrix(c(0, 0, 0, 1, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype3 <- matrix(c(0, 0, 0, 0, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype4 <- matrix(c(1, 1, 1, 1, 1, 1, 1), nrow = Ntip(tree), ncol = 1)
+genotype5 <- matrix(c(0, 1, 1, 1, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype6 <- matrix(c(0, 1, 1, 1, 1, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype7 <- matrix(c(0, 1, 0, 0, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype8 <- matrix(c(0, 0, 0, 1, 0, 0, 0), nrow = Ntip(tree), ncol = 1)
+genotype <- cbind(genotype1, genotype2, genotype3, genotype4, genotype5, genotype6, genotype7, genotype8)
+row.names(genotype) <- tree$tip.label
+colnames(genotype) <- c("SNP1", "SNP2", "SNP3", "SNP4", "SNP5", "SNP6", "SNP7", "SNP8")
+
+snp_gene_key <- matrix(NA, nrow = ncol(genotype), ncol = 2)
+colnames(snp_gene_key) <- c("SNP", "GENE")
+snp_gene_key[ , 1] <- c("SNP1", "SNP2", "SNP3", "SNP4", "SNP5", "SNP6", "SNP7", "SNP8")
+snp_gene_key[ , 2] <- c("GENE1", "GENE2", "GENE3", "GENE4", "GENE5", "GENE6", "GENE7", "GENE7")
+
+write.table(genotype, file = "/nfs/esnitkin/bin_group/pipeline/Github/gwas/microbeGWAS/dummy_data/discrete_phenotype_grouped_genotype/grouped_genotype.tsv", sep = "\t", quote = FALSE, row.names = TRUE, col.names = TRUE)
+write.table(discrete_phenotype, file = "/nfs/esnitkin/bin_group/pipeline/Github/gwas/microbeGWAS/dummy_data/discrete_phenotype_grouped_genotype/discrete_phenotype.tsv", sep = "\t", quote = FALSE, row.names = TRUE, col.names = TRUE)
+write.table(snp_gene_key, file = "/nfs/esnitkin/bin_group/pipeline/Github/gwas/microbeGWAS/dummy_data/discrete_phenotype_grouped_genotype/snp_gene_key.tsv", sep = "\t", quote = FALSE, row.names = TRUE, col.names = TRUE)
+write.tree(tree, file = "/nfs/esnitkin/bin_group/pipeline/Github/gwas/microbeGWAS/dummy_data/discrete_phenotype_grouped_genotype/tree.tree")
