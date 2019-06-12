3574a607f035df0ea879df88f9b6827ac99629f9
katiembrennan <ktbrennan3@gmail.com>
2019-06-12 16:58:37 -0400

    Updated syntax with lintr guidelines

--- microbeGWAS/R/continuous_algorithm.R
+++ microbeGWAS/R/continuous_algorithm.R
@@ -1,17 +1,20 @@
 run_ks_test <- function(t_index, non_t_index, phenotype_by_edges){
-  # TODO deal with cases when there isn't enough data (aren't at least 1 of least t_index and non_t_index)
-  # TODO -- If i were really fancy -- I would remove these from the genotype and then report that they're bad and continue without breaking out of the algorithm
-  # right now it will cause a stop error --- which won't be good for lots of things.
-  # Can't seem to recapitulate the error when there are not enough samples to run the test,but all indices are list of at least one. Hmm.
+  # TODO deal with cases when there isn't enough data
+  #   (aren't at least 1 of least t_index and non_t_index)
+  # TODO -- If i were really fancy -- I would remove these from the genotype and
+  # report that they're bad & continue without breaking out of the algorithm
+  # right now it will cause a stop error which won't be good for lots of things.
+  # Can't seem to recapitulate the error when there are not enough samples to
+  # run the test,but all indices are list of at least one. Hmm.
 
   # Function description -------------------------------------------------------
   # Run a Kolmogorov-Smirnov test on the continuous phenotype. The phenotype is
   # divided into two groups: transition edges and non-transition edges.
   #
   # Input:
-  # t_index.     Vector. Each number is the index phenotype transition edges on the tree.
-  # non_t_index. Vector.  Each number is the index phenotype transition edges on the tree.
-  # phenotype_by_edges. Numeric matrix. Phenotype values are each node stored in a matrix. Each row corresponds to an edge. 1st column is parent node. 2nd column is daughter node.
+  # t_index: Vector. Each number is the index phenotype transition edges on the tree.
+  # non_t_index: Vector. Each number is the index phenotype transition edges on the tree.
+  # phenotype_by_edges: Numeric matrix. Phenotype values are each node stored in a matrix. Each row corresponds to an edge. 1st column is parent node. 2nd column is daughter node.
   #
   # Outputs:
   # A list of 4 elements:
@@ -41,16 +44,20 @@ run_ks_test <- function(t_index, non_t_index, phenotype_by_edges){
 
 
   # Function -------------------------------------------------------------------
-  p_trans_delta     <- calculate_phenotype_change_on_edge(t_index,     phenotype_by_edges)
-  p_non_trans_delta <- calculate_phenotype_change_on_edge(non_t_index, phenotype_by_edges)
+  p_trans_delta <-
+    calculate_phenotype_change_on_edge(t_index,     phenotype_by_edges)
+  p_non_trans_delta <-
+    calculate_phenotype_change_on_edge(non_t_index, phenotype_by_edges)
 
-  # withCalligHandlers suppresses the warning message: 'cannot compute exact p-value with ties'
+  # withCalligHandlers suppresses the warning message:
+  # 'cannot compute exact p-value with ties'
   set.seed(1)
   ks_results <-  withCallingHandlers(ks.test(p_trans_delta, p_non_trans_delta),
                       warning = function(w) {
-                        if (grepl("cannot compute exact p-value with ties", w$message))
+                        if (grepl("cannot compute exact p-value with ties",
+                                  w$message))
                           invokeRestart("muffleWarning")
-                      } )
+                      })
 
   # Return output --------------------------------------------------------------
   results <- list("pval"      = round(ks_results$p.value, digits = 20),
@@ -64,7 +71,8 @@ get_hi_conf_tran_indices <- function(geno_tran, geno_conf, index, tr){
   # Function description -------------------------------------------------------
   #
   # Input:
-  # geno_tran. List of lists. Length(genotype_transition_list) == number of genotypes.
+  # geno_tran. List of lists.
+  #            Length(genotype_transition_list) == number of genotypes.
   #            For each genotype there are two lists: $transition and $trans_dir
   #            Length($transition) == length($trans_dir) == Nedge(tr).
   #            Each of these are either 0, 1, or -1.
@@ -76,12 +84,11 @@ get_hi_conf_tran_indices <- function(geno_tran, geno_conf, index, tr){
   #
   # Outputs:
   # results. List of 2.
-  #   "trans_index" = hi_conf_trans_index. Vector of numbers. Each number
-  #                                        corresponds to an edge that is both a
-  #                                       transition and high confidence.
-  #   "non_trans_index" = hi_conf_non_trans_index. Vector of numbers. Each number
-  #                                        corresponds to an edge that is both a
-  #                                        not a transition and high confidence.
+  #   trans_index: Vector of numbers. Each number corresponds to an edge that is
+  #                both a transition and high confidence.
+  #   non_trans_index: hi_conf_non_trans_index. Vector of numbers. Each number
+  #                    corresponds to an edge that is both a not a transition
+  #                    and high confidence.
   #
   # Check input ----------------------------------------------------------------
   check_equal(length(geno_tran), length(geno_conf))
@@ -100,8 +107,10 @@ get_hi_conf_tran_indices <- function(geno_tran, geno_conf, index, tr){
   # THESE EDGES ARE DEFINED BY THE NODES IN THE CORRESPONDING ROWS OF tr$EDGE
 
   # SUBSET TRANSITION / NON-TRANSITION EDGES TO ONLY HIGH CONFIDENCE ONES
-  hi_conf_trans_index     <- trans_index[    as.logical(geno_conf[[index]][trans_index    ])]
-  hi_conf_non_trans_index <- non_trans_index[as.logical(geno_conf[[index]][non_trans_index])]
+  hi_conf_trans_index <-
+    trans_index[as.logical(geno_conf[[index]][trans_index])]
+  hi_conf_non_trans_index <-
+    non_trans_index[as.logical(geno_conf[[index]][non_trans_index])]
 
   # Return output --------------------------------------------------------------
   return(list("trans_index" = hi_conf_trans_index,
@@ -158,7 +167,8 @@ continuous_permutation <- function(index_obj, tr, geno_conf, perm, num_i){
     permuted_trans_index_mat[j, ] <- sample(1:num_hi_conf_edges,
                                         size = num_trans_edges,
                                         replace = TRUE,
-                                        prob = tr$edge.length[hi_conf_edges]/sum(tr$edge.length[hi_conf_edges]))
+                                        prob = tr$edge.length[hi_conf_edges] /
+                                          sum(tr$edge.length[hi_conf_edges]))
   } # end for (j)
 
   # permuted_trans_index_mat is my new, permuted "indices$trans_index" where
@@ -173,39 +183,60 @@ continuous_permutation <- function(index_obj, tr, geno_conf, perm, num_i){
   # each variable means.
 
   # Check and return output ----------------------------------------------------
-  check_dimensions(permuted_trans_index_mat, exact_rows = perm, min_rows = perm, exact_cols = NULL, min_cols = 1)
+  check_dimensions(permuted_trans_index_mat,
+                   exact_rows = perm,
+                   min_rows = perm,
+                   exact_cols = NULL,
+                   min_cols = 1)
   return(list("hi_conf_edges" = hi_conf_edges,
               "permuted_trans_index_mat" = permuted_trans_index_mat))
 }
 
-calculate_empirical_pheno_delta <- function(perm, permuted_trans_mat, hi_conf_edge_nums, pheno_by_edges){
+calculate_empirical_pheno_delta <- function(perm,
+                                            permuted_trans_mat,
+                                            hi_conf_edge_nums,
+                                            pheno_by_edges){
   # Calculate permuted (aka empirical) pheno deltas
   empirical_ks_stat <- rep(NA, perm)
   for (k in 1:perm) {
     permuted_trans_index     <- unique(permuted_trans_mat[k, ])
-    permuted_non_trans_index <- c(1:length(hi_conf_edge_nums))[!(c(1:length(hi_conf_edge_nums)) %in% unique(permuted_trans_mat[k, ]))]
-    empirical_results        <- run_ks_test(permuted_trans_index, permuted_non_trans_index, pheno_by_edges)
+    permuted_non_trans_index <-
+      c(1:length(hi_conf_edge_nums))[!(c(1:length(hi_conf_edge_nums))
+                                       %in% unique(permuted_trans_mat[k, ]))]
+    empirical_results        <- run_ks_test(permuted_trans_index,
+                                            permuted_non_trans_index,
+                                            pheno_by_edges)
     empirical_ks_stat[k]     <- empirical_results$statistic
   } # end for (k)
   return(empirical_ks_stat)
 }
 
-calculate_genotype_significance <- function(mat, permutations, genotype_transition_list, tr, pheno_recon_ordered_by_edges, genotype_confidence, genotype_reconstruction){
+calculate_genotype_significance <- function(mat,
+                                            permutations,
+                                            genotype_transition_list,
+                                            tr,
+                                            pheno_recon_ordered_by_edges,
+                                            genotype_confidence,
+                                            genotype_reconstruction){
   # Function description -------------------------------------------------------
   #
   # Input:
-  # mat. Matrix. Nrow(mat) == number of samples. Each column is a variant. Matrix is binary.
-  # permutations. Integer. Number of times to run the permutation test.
-  # genotype_transition_list. List of lists. Length(genotype_transition_list) == number of genotypes.
-  #                           For each genotype there are two lists: $transition and $trans_dir
-  #                           Length($transition) == length($trans_dir) == Nedge(tr).
+  # mat: Matrix. Nrow(mat) == number of samples. Each column is a variant.
+  #      Matrix is binary.
+  # permutations: Integer. Number of times to run the permutation test.
+  # genotype_transition_list: List of lists.
+  #                           Length(genotype_transition_list) == num genotypes.
+  #                           For each genotype there are two lists:
+  #                           $transition & $trans_dir
+  #                           Length($transition) == length($trans_dir) ==
+  #                           Nedge(tr).
   #                           Each of these are either 0, 1, or -1.
-  # tr. Phylo.
-  # pheno_recon_ordered_by_edges.  Matrix. Nrow = Nedge(tr). Ncol = 2. Values
+  # tr: Phylo.
+  # pheno_recon_ordered_by_edges: Matrix. Nrow = Nedge(tr). Ncol = 2. Values
   #                                   are the phenotype reconstruction at each
   #                                   node, as ordered by edges. It's the same
   #                                   organization as tr$edge.
-  # genotype_confidence. List. Length(list) = ncol(mat) == number of genotypes.
+  # genotype_confidence: List. Length(list) = ncol(mat) == number of genotypes.
   #                      Each entry is a vector with length == Nedge(tr). All
   #                      entries are 0 (low confidence) or 1 (high confidence).
   #
@@ -236,29 +267,47 @@ calculate_genotype_significance <- function(mat, permutations, genotype_transiti
   num_genotypes <- ncol(mat)
   pvals <- trans_median <- all_edges_median <- observed_ks_stat <- rep(NA, num_genotypes)
   names(pvals) <- colnames(mat)
-  empirical_ks_stat_list <- observed_pheno_trans_delta <- observed_pheno_non_trans_delta <- rep(list(0), num_genotypes)
+  empirical_ks_stat_list <-
+    observed_pheno_trans_delta <-
+    observed_pheno_non_trans_delta <- rep(list(0), num_genotypes)
 
   for (i in 1:num_genotypes) {
-    indices <- get_hi_conf_tran_indices(genotype_transition_list, genotype_confidence, i, tr)
+    indices <- get_hi_conf_tran_indices(genotype_transition_list,
+                                        genotype_confidence,
+                                        i,
+                                        tr)
 
-    # Run KS test to find out if the phenotype change on transition edges is significantly different from phenotype change on non-transition edges
-    observed_results <- run_ks_test(indices$trans_index, indices$non_trans_index, pheno_recon_ordered_by_edges)
+    # Run KS test to find out if the phenotype change on transition edges is
+    # significantly different from phenotype change on non-transition edges
+    observed_results <- run_ks_test(indices$trans_index,
+                                    indices$non_trans_index,
+                                    pheno_recon_ordered_by_edges)
 
     # Save these for reporting / plots
-    observed_pheno_non_trans_delta[[i]] <- observed_results$pheno_non_trans_delta
-    observed_pheno_trans_delta[[i]]     <- observed_results$pheno_trans_delta
-    observed_ks_stat[i]                 <- observed_results$statistic
-    trans_median[i]                     <- median(observed_results$pheno_trans_delta)
-    all_edges_median[i]                 <- median(c(observed_results$pheno_trans_delta, observed_results$pheno_non_trans_delta))
+    observed_pheno_non_trans_delta[[i]] <-
+      observed_results$pheno_non_trans_delta
+    observed_pheno_trans_delta[[i]] <- observed_results$pheno_trans_delta
+    observed_ks_stat[i] <- observed_results$statistic
+    trans_median[i] <- median(observed_results$pheno_trans_delta)
+    all_edges_median[i] <- median(c(observed_results$pheno_trans_delta,
+                                    observed_results$pheno_non_trans_delta))
 
     # Create permuted transition index matrix and get edge numbersof the high confidence edges
-    perm_results <- continuous_permutation(indices, tr, genotype_confidence, permutations, i)
+    perm_results <-
+      continuous_permutation(indices, tr, genotype_confidence, permutations, i)
 
     # Calculate permuted (aka empirical) pheno deltas
-    empirical_ks_stat <- calculate_empirical_pheno_delta(permutations, perm_results$permuted_trans_index_mat, perm_results$hi_conf_edges, pheno_recon_ordered_by_edges)
+    empirical_ks_stat <-
+      calculate_empirical_pheno_delta(permutations,
+                                      perm_results$permuted_trans_index_mat,
+                                      perm_results$hi_conf_edges,
+                                      pheno_recon_ordered_by_edges)
 
-    # empirical p value calculation here: (1 + more extreme observations) / (1 + permutations)
-    pvals[i] <- calculate_permutation_based_p_value(empirical_ks_stat, observed_ks_stat[i], permutations)
+    # empirical p value calculation here:
+    # (1 + more extreme observations) / (1 + permutations)
+    pvals[i] <- calculate_permutation_based_p_value(empirical_ks_stat,
+                                                    observed_ks_stat[i],
+                                                    permutations)
 
     # Save these for reporting / plots
     empirical_ks_stat_list[[i]] <- empirical_ks_stat
@@ -267,9 +316,12 @@ calculate_genotype_significance <- function(mat, permutations, genotype_transiti
   # Return output --------------------------------------------------------------
   results <- list("pvals" = pvals, "ks_statistics" = empirical_ks_stat_list,
                   "observed_pheno_trans_delta" = observed_pheno_trans_delta,
-                  "observed_pheno_non_trans_delta" = observed_pheno_non_trans_delta,
-                  "trans_median" = trans_median, "all_edges_median" = all_edges_median,
-                  "num_genotypes" = ncol(mat), "observed_ks_stat" = observed_ks_stat) # 2018-11-28
+                  "observed_pheno_non_trans_delta" =
+                    observed_pheno_non_trans_delta,
+                  "trans_median" = trans_median,
+                  "all_edges_median" = all_edges_median,
+                  "num_genotypes" = ncol(mat),
+                  "observed_ks_stat" = observed_ks_stat)
   return(results)
 } # end calculate_genotype_significance()
 
@@ -293,14 +345,15 @@ get_sig_hits_while_correcting_for_multiple_testing <- function(hit_values, fdr){
   check_num_between_0_and_1(fdr)
 
   # Function -------------------------------------------------------------------
-  fdr_corrected_pvals            <- p.adjust(hit_values, method = "fdr")
-  sig_pvals                      <- as.data.frame(fdr_corrected_pvals[fdr_corrected_pvals < fdr])
-  sig_pvals                      <- as.data.frame(sig_pvals)
-  row.names(sig_pvals)           <- names(hit_values)[fdr_corrected_pvals < fdr]
-  fdr_corrected_pvals            <- as.data.frame(fdr_corrected_pvals)
+  fdr_corrected_pvals <- p.adjust(hit_values, method = "fdr")
+  sig_pvals <- as.data.frame(fdr_corrected_pvals[fdr_corrected_pvals < fdr])
+  sig_pvals <- as.data.frame(sig_pvals)
+  row.names(sig_pvals) <- names(hit_values)[fdr_corrected_pvals < fdr]
+  fdr_corrected_pvals <- as.data.frame(fdr_corrected_pvals)
   row.names(fdr_corrected_pvals) <- names(hit_values)
 
   # Check and return output ----------------------------------------------------
-  results                        <- list("hit_pvals" = fdr_corrected_pvals, "sig_pvals" = sig_pvals)
+  results <- list("hit_pvals" = fdr_corrected_pvals,
+                  "sig_pvals" = sig_pvals)
   return(results)
 } # end get_sig_hits_while_correcting_for_multiple_testing
