c1badf89138cee4b25e88bfc740d49f9ab28925c
katiembrennan <ktbrennan3@gmail.com>
2019-06-11 20:30:01 -0400

    Moved description of R tree structure from deprecated file phyc_lib.R to transition_edges.R

--- microbeGWAS/R/transition_edges.R
+++ microbeGWAS/R/transition_edges.R
@@ -1,3 +1,15 @@
+# Note on phylogenetic tree structure
+# tr$edge:
+#       [,1] [,2]
+# [1,]  111  112
+# [2,]  112  113
+# [3,]  113  114
+# [4,]  114  115
+# Dim = 2*(Ntip(tr)) x 2
+# Each row is an edge.
+# The first colum is the older node and the second column is the younger node.
+
+
 identify_transition_edges <- function(tr, mat, num, node_recon, disc_cont){
   # Function description -------------------------------------------------------
   # Given a reconstruction identify which edges on tree are transitions.
