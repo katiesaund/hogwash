98c6b2f0928476f2b455d9f456eca57b676fe8e9
katiembrennan <ktbrennan3@gmail.com>
2019-06-13 22:01:56 -0400

    new size for tree image

--- dummy_data/make_tree_image_for_wiki.R
+++ dummy_data/make_tree_image_for_wiki.R
@@ -3,6 +3,6 @@ temp <- rtree(4)
 temp$tip.label <- c("sample_1", "sample_2", "sample_3", "sample_4")
 plot(temp, use.edge.length = FALSE, edge.width = 5, cex = 2, no.margin = TRUE)
 
-jpeg("dummy_data/tree_for_wiki.jpg", width = 250, height = 150)
+jpeg("dummy_data/tree_for_wiki.jpg", width = 250, height = 250)
 plot(temp, use.edge.length = FALSE, edge.width = 5, cex = 2, no.margin = TRUE)
 dev.off()
