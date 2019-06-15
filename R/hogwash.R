388c59da9a542e4bbbb7c3c6b3417bcf7a005f2c
katiembrennan <ktbrennan3@gmail.com>
2019-06-15 17:04:43 -0400

    Updated hogwash.R to work with data loaded into R workspace, rather be run from the command line.

--- R/hogwash.R
+++ R/hogwash.R
@@ -1,35 +1,28 @@
 hogwash <- function(pheno,
                     geno,
                     tree,
-                    file_name,
-                    dir,
+                    file_name = paste(Sys.Date(), "_hogwash", sep = ""),
+                    dir = ".",
                     perm = 10000,
                     fdr = 0.15,
                     bootstrap = 0.70,
-                    annotation = NULL,
                     group_genotype_key = NULL){
-
-  args                        <- NULL
-  args$test                   <- FALSE
-  args$phenotype              <- read_in_tsv_matrix(pheno)
-  args$genotype               <- read_in_tsv_matrix(geno)
-  args$tree                   <- read.tree(tree)
-  args$tree$node.label[1]     <- 0
-  args$tree$node.label        <- as.numeric(args$tree$node.label)
-  args$output_name            <- file_name
-  args$output_dir             <- dir
-  args$perm                   <- as.numeric(perm) #typically 10,000
-  args$fdr                    <- as.numeric(fdr)
-  args$annot                  <- NULL
-  if (!is.null(annotation)) {
-   args$annot                 <- read_in_tsv_matrix(annotation)
+  args <- NULL
+  args$phenotype <- pheno
+  args$genotype <- geno
+  args$tree <- tree
+  args$output_name <- file_name
+  args$output_dir <- dir
+  args$perm <- perm
+  args$fdr <- fdr
+  args$bootstrap_cutoff <- bootstrap
+  args$annot <- NULL
+  args$gene_snp_lookup <- group_genotype_key
+  group_genotype <- FALSE
+  if (!is.null(group_genotype_key)){
+    group_genotype <- TRUE
   }
   args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr, args$annot)
-  args$bootstrap_cutoff       <- as.numeric(bootstrap)
-  args$group_genotype         <- NULL
-  if (!is.null(args$group_genotype)) {
-    args$gene_snp_lookup      <- read_in_tsv_matrix(group_genotype_key)
-  }
   select_test_type(args)
 }
 
