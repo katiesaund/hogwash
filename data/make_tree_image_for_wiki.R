965457cca31002194472c0ac9b24031fe153c5db
katiembrennan <ktbrennan3@gmail.com>
2019-06-16 02:26:49 -0400

    commented out the code in this function because plot was causing package not to build. I don't know why. To come back to

--- data/make_tree_image_for_wiki.R
+++ data/make_tree_image_for_wiki.R
@@ -1,8 +1,8 @@
 set.seed(1)
 temp <- ape::rtree(4)
 temp$tip.label <- c("sample_1", "sample_2", "sample_3", "sample_4")
-plot(temp, use.edge.length = FALSE, edge.width = 5, cex = 2, no.margin = TRUE)
+# plot(temp, use.edge.length = FALSE, edge.width = 5, cex = 2, no.margin = TRUE)
 
-jpeg("/nfs/esnitkin/bin_group/pipeline/Github/gwas/hogwash/data/tree_for_wiki.jpg", width = 400, height = 400)
-plot(temp, use.edge.length = FALSE, edge.width = 5, cex = 2, no.margin = TRUE)
-dev.off()
+# jpeg("/nfs/esnitkin/bin_group/pipeline/Github/gwas/hogwash/data/tree_for_wiki.jpg", width = 400, height = 400)
+# plot(temp, use.edge.length = FALSE, edge.width = 5, cex = 2, no.margin = TRUE)
+# dev.off()
