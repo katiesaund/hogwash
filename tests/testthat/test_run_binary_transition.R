2be39f39107fd5fe787f7252063a58197043bbf4
katiembrennan <ktbrennan3@gmail.com>
2019-06-17 00:02:17 -0400

    Fixed relative path issue by removing relative path with current directory. Odd bug introduced when I updated test_run_continuous.R - tbd.

--- tests/testthat/test_run_binary_transition.R
+++ tests/testthat/test_run_binary_transition.R
@@ -3,34 +3,23 @@ library(ape)
 context("Run binary transition") #-----------------------------------------------#
 
 test_that("run_binary_transition() doesn't give any errors when given a discrete phenotype and snps that group into genes" ,{
-  test_dir <- "../../data/outputs/"
-  test_pheno <- "../../data/discrete_phenotype.tsv"
-  test_tree  <- "../../data/tree.tree"
-  test_geno  <- "../../data/grouped_genotype.tsv"
-  test_gene_snp_lookup <- "../../data/snp_gene_key.tsv"
-  test_annot <- NULL
-  test_name  <- "dummy_group"
-  test_perm  <- "1000"
-  test_fdr <- "0.15"
-  test_bootstrap <- "0.7"
-  test_group_genotype <- TRUE
   args                        <- NULL
   args$test                   <- FALSE
-  args$phenotype              <- read_in_tsv_matrix(test_pheno)
-  args$tree                   <- ape::read.tree(test_tree)
+  args$phenotype              <- hogwash::antibiotic_resistance
+  args$tree                   <- hogwash::tree
   args$tree$node.label[1]     <- 0
   args$tree$node.label        <- as.numeric(args$tree$node.label)
-  args$genotype               <- read_in_tsv_matrix(test_geno)
-  args$output_name            <- test_name # Ex: log_toxin_snp_stop
-  args$output_dir             <- test_dir # Directory in which all output files will be saved
-  args$perm                   <- as.numeric(test_perm) #typically 10,000
-  args$fdr                    <- as.numeric(test_fdr)
-  args$annotation             <- NULL #read_in_tsv_matrix(test_annot)
-  args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr, args$annot)
-  args$bootstrap_cutoff       <- as.numeric(test_bootstrap)
-  args$group_genotype         <- test_group_genotype
+  args$genotype               <- hogwash::genotype
+  args$output_name            <- "dummy_grouped"
+  args$output_dir             <- "."
+  args$perm                   <- 1000
+  args$fdr                    <- 0.15
+  args$annotation             <- NULL
+  args$discrete_or_continuous <- "discrete"
+  args$bootstrap_cutoff       <- 0.7
+  args$group_genotype         <- TRUE
   if (args$group_genotype ) {
-    args$gene_snp_lookup      <- read_in_tsv_matrix(test_gene_snp_lookup)
+    args$gene_snp_lookup      <- hogwash::snp_gene_key
   } else {
     args$gene_snp_lookup <- NULL
   }
@@ -38,37 +27,22 @@ test_that("run_binary_transition() doesn't give any errors when given a discrete
 })
 
 test_that("run_binary_transition() doesn't give any errors when given a discrete phenotype and snps (do not group into genes)" ,{
-  test_dir <- "../../data/outputs/"
-  test_pheno <- "../../data/discrete_phenotype.tsv"
-  test_tree  <- "../../data/tree.tree"
-  test_geno  <- "../../data/grouped_genotype.tsv"
-  test_gene_snp_lookup <- NULL
-  test_annot <- NULL
-  test_name  <- "dummy"
-  test_perm  <- "1000"
-  test_fdr <- "0.15"
-  test_bootstrap <- "0.7"
-  test_group_genotype <- FALSE
   args                        <- NULL
   args$test                   <- FALSE
-  args$phenotype              <- read_in_tsv_matrix(test_pheno)
-  args$tree                   <- ape::read.tree(test_tree)
+  args$phenotype              <- hogwash::antibiotic_resistance
+  args$tree                   <- hogwash::tree
   args$tree$node.label[1]     <- 0
   args$tree$node.label        <- as.numeric(args$tree$node.label)
-  args$genotype               <- read_in_tsv_matrix(test_geno)
-  args$output_name            <- test_name # Ex: log_toxin_snp_stop
-  args$output_dir             <- test_dir # Directory in which all output files will be saved
-  args$perm                   <- as.numeric(test_perm) #typically 10,000
-  args$fdr                    <- as.numeric(test_fdr)
-  args$annotation             <- NULL #read_in_tsv_matrix(test_annot)
-  args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr, args$annot)
-  args$bootstrap_cutoff       <- as.numeric(test_bootstrap)
-  args$group_genotype         <- test_group_genotype
-  if (args$group_genotype ) {
-    args$gene_snp_lookup      <- read_in_tsv_matrix(test_gene_snp_lookup)
-  } else {
-    args$gene_snp_lookup <- NULL
-  }
+  args$genotype               <- hogwash::genotype
+  args$output_name            <- "dummy_grouped"
+  args$output_dir             <- "."
+  args$perm                   <- 1000
+  args$fdr                    <- 0.15
+  args$annotation             <- NULL
+  args$discrete_or_continuous <- "discrete"
+  args$bootstrap_cutoff       <- 0.7
+  args$group_genotype         <- FALSE
+  args$gene_snp_lookup <- NULL
   expect_error(run_binary_transition(args), NA)
 })
 
--- tests/testthat/test_run_continuous.R
+++ tests/testthat/test_run_continuous.R
@@ -1,69 +1,43 @@
 context("Run continuous") #----------------------------------------------------#
 
 test_that("run_continuous() doesn't give any errors when given a continuous phenotype and snps (do not group into genes)" ,{
-  test_dir <- "../../data/outputs/"
-  test_pheno <- "../../data/continuous_phenotype.tsv"
-  test_tree  <- "../../data/tree.tree"
-  test_geno  <- "../../data/grouped_genotype.tsv"
-  test_gene_snp_lookup <- NULL
-  test_annot <- NULL
-  test_name  <- "dummy_group"
-  test_perm  <- "1000"
-  test_fdr <- "0.15"
-  test_bootstrap <- "0.7"
-  test_group_genotype <- FALSE
   args                        <- NULL
   args$test                   <- FALSE
-  args$phenotype              <- read_in_tsv_matrix(test_pheno)
-  args$tree                   <- ape::read.tree(test_tree)
+  args$phenotype              <- hogwash::growth
+  args$tree                   <- hogwash::tree
   args$tree$node.label[1]     <- 0
   args$tree$node.label        <- as.numeric(args$tree$node.label)
-  args$genotype               <- read_in_tsv_matrix(test_geno)
-  args$output_name            <- test_name # Ex: log_toxin_snp_stop
-  args$output_dir             <- test_dir # Directory in which all output files will be saved
-  args$perm                   <- as.numeric(test_perm) #typically 10,000
-  args$fdr                    <- as.numeric(test_fdr)
-  args$annotation             <- NULL #read_in_tsv_matrix(test_annot)
-  args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr, args$annot)
-  args$bootstrap_cutoff       <- as.numeric(test_bootstrap)
-  args$group_genotype         <- test_group_genotype
-  if (args$group_genotype ) {
-    args$gene_snp_lookup      <- read_in_tsv_matrix(test_gene_snp_lookup)
-  } else {
-    args$gene_snp_lookup <- NULL
-  }
+  args$genotype               <- hogwash::genotype
+  args$output_name            <- "dummy_grouped"
+  args$output_dir             <- "."
+  args$perm                   <- 1000
+  args$fdr                    <- 0.15
+  args$annotation             <- NULL
+  args$discrete_or_continuous <- "continuous"
+  args$bootstrap_cutoff       <- 0.7
+  args$group_genotype         <- FALSE
+  args$gene_snp_lookup <- NULL
   expect_error(run_continuous(args), NA)
 })
 
 test_that("run_continuous() doesn't give any errors when given a continuous phenotype and snps are grouped into genes" ,{
-  test_dir <- "../../data/outputs/"
-  test_pheno <- "../../data/continuous_phenotype.tsv"
-  test_tree  <- "../../data/tree.tree"
-  test_geno  <- "../../data/grouped_genotype.tsv"
-  test_gene_snp_lookup <- "../../data/snp_gene_key.tsv"
-  test_annot <- NULL
-  test_name  <- "dummy"
-  test_perm  <- "1000"
-  test_fdr <- "0.15"
-  test_bootstrap <- "0.7"
-  test_group_genotype <- TRUE
   args                        <- NULL
   args$test                   <- FALSE
-  args$phenotype              <- read_in_tsv_matrix(test_pheno)
-  args$tree                   <- ape::read.tree(test_tree)
+  args$phenotype              <- hogwash::growth
+  args$tree                   <- hogwash::tree
   args$tree$node.label[1]     <- 0
   args$tree$node.label        <- as.numeric(args$tree$node.label)
-  args$genotype               <- read_in_tsv_matrix(test_geno)
-  args$output_name            <- test_name # Ex: log_toxin_snp_stop
-  args$output_dir             <- test_dir # Directory in which all output files will be saved
-  args$perm                   <- as.numeric(test_perm) #typically 10,000
-  args$fdr                    <- as.numeric(test_fdr)
-  args$annotation             <- NULL #read_in_tsv_matrix(test_annot)
-  args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr, args$annot)
-  args$bootstrap_cutoff       <- as.numeric(test_bootstrap)
-  args$group_genotype         <- test_group_genotype
+  args$genotype               <- hogwash::genotype
+  args$output_name            <- "dummy_grouped"
+  args$output_dir             <- "."
+  args$perm                   <- 1000
+  args$fdr                    <- 0.15
+  args$annotation             <- NULL
+  args$discrete_or_continuous <- "continuous"
+  args$bootstrap_cutoff       <- 0.7
+  args$group_genotype         <- TRUE
   if (args$group_genotype ) {
-    args$gene_snp_lookup      <- read_in_tsv_matrix(test_gene_snp_lookup)
+    args$gene_snp_lookup      <- hogwash::snp_gene_key
   } else {
     args$gene_snp_lookup <- NULL
   }
