6abe937a1fdd6e41512ad5f0c2f1ded44b7f2f40
katiembrennan <ktbrennan3@gmail.com>
2019-06-19 21:02:41 -0400

    Added documentation for all exported data objects.

--- data/data.R
+++ data/data.R
@@ -1,4 +1,25 @@
 #' Binary phenotype
 #'
 #' An example binary phenotype matrix.
-"discrete_phenotype"
+"antibiotic_resistance"
+
+#' Continuous phenotype
+#'
+#' An example continous phenotype matrix.
+"growth"
+
+#' Key
+#'
+#' An example mapping SNPs to genes.
+"snp_gene_key"
+
+#' Binary genotype
+#'
+#' An example binary genotype matrix.
+"snp_genotype"
+
+#' Phylogenetic tree
+#'
+#' An example tree.
+"tree"
+
