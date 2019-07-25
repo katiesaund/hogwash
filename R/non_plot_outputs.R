236cbab126f6611f9f1e3684be48c6128fa901bb
katiembrennan <ktbrennan3@gmail.com>
2019-07-25 13:10:52 -0400

    Updated comments to roxygen2 style.

--- R/non_plot_outputs.R
+++ R/non_plot_outputs.R
@@ -1,28 +1,54 @@
-
+#' save_continuous
+#'
+#' @description Save the results of the Continuous Test in an rdata object
+#'  called hogwash_continuous.
+#' @param hogwash_continuous List of objects.
+#' @param file_name Character.
+#'
+#' @noRd
 save_continuous <- function(hogwash_continuous, file_name){
   save(hogwash_continuous, file = file_name)
-}
+} # end save_continuous()
+
+#' save_synchronous
+#'
+#' @description Save the results of the Synchronous Test in an rdata object
+#'  called hogwash_synchronous
+#'
+#' @param hogwash_synchronous List of objects.
+#' @param file_name Character.
+#'
+#' @noRd
 save_synchronous <- function(hogwash_synchronous, file_name){
   save(hogwash_synchronous, file = file_name)
-}
+} # end save_synchronous()
+
+#' save_convergence
+#'
+#' @description Save the results of the PhyC Test in an rdata object
+#'  called hogwash_synchronous
+#'
+#' @param hogwash_phyc List of objects.
+#' @param file_name Character.
+#'
+#' @noRd
 save_convergence <- function(hogwash_phyc, file_name){
   save(hogwash_phyc, file = file_name)
-}
+} # end save_convergence()
 
+#' save_results_as_r_object
+#'
+#' @description Save all of the non-plot outputs in a .rda file.
+#'
+#' @param dir String. Path to directory where file should be saved.
+#' @param name String. Name of file without suffix.
+#' @param object  List of various inputs to save.
+#' @param prefix Character. Test type (continuous, synchronous, convergence)
+#' @param group_logical  Logical. Whether or not genotypes were grouped.
+#'
+#' @noRd
 save_results_as_r_object <- function(dir, name, object, prefix, group_logical){
-  # Function description -------------------------------------------------------
-  # Save all of the non-plot outputs in a .rda file.
-  #
-  # Inputs:
-  # dir. String. Path to directory where file should be saved.
-  # name. String. Name of file without suffix.
-  # object. List of various inputs to save.
-  # prefix. Character. Test type (continuous, synchronous, convergence)
-  # group_logical. Logical. Whether or not genotypes were grouped.
-  #
-  # Output:
-  # .rda file.
-  #
+  # TODO Chance convergence to phyc
   # Check inputs ---------------------------------------------------------------
   check_is_string(dir)
   check_if_dir_exists(dir)
@@ -33,10 +59,8 @@ save_results_as_r_object <- function(dir, name, object, prefix, group_logical){
   # Function & output ----------------------------------------------------------
   if (group_logical) {
     fname <- paste0(dir, "/hogwash_", prefix, "_grouped_", name, ".rda")
-    # save(object, file = paste0(dir, "/hogwash_", prefix, "_grouped_", name, ".rda"))
   } else {
     fname <- paste0(dir, "/hogwash_", prefix, "_", name, ".rda")
-    # save(object, file = paste0(dir, "/hogwash_", prefix, "_", name, ".rda"))
   }
 
   if (prefix == "continuous") {
@@ -48,8 +72,4 @@ save_results_as_r_object <- function(dir, name, object, prefix, group_logical){
   } else {
     stop("test name incorrect")
   }
-
 } # end save_results_as_r_object()
-
-
-# End of script ----------------------------------------------------------------
