2a938c2d227b74753ff2beaf187201ce9d462f7c
katiembrennan <ktbrennan3@gmail.com>
2019-07-25 12:54:59 -0400

    Added formatting to roxygen documentation and shorted some lines to fit within 80 characters.

--- R/hogwash.R
+++ R/hogwash.R
@@ -4,88 +4,96 @@
 #'   runs either the Continuous Test (when given continuous phenotype data) or
 #'   both the Synchronous Test and PhyC (when given binary phenotype data).
 #'
-#' @details Overview: hogwash reads in one phenotype (either continuous or binary), a
-#'   matrix of binary genotypes, and a phylogenetic tree. Given these inputs it
-#'   performs an ancestral reconstruction of that phenotype and each genotype.
-#'   The ancestral reconstructions are used to perform one of several tests to
-#'   associate the the genotypes with the phenotype:
-#'     1. Continuous Test
-#'     2. Synchronous Test
-#'     3. PhyC (Farhat et al.)
-#'   Once a test finishes running it returns (i) p-values for all genotypes
-#'   tested, (ii) a manhattan plot of those p-values; if any of the genotypes
-#'   tested were significant associated with the phenotype after FDR correction
-#'   it also returns (iii) a list of significant hits and (iv) figures of the
-#'   genotype & phenotype reconstructions on the tree.
+#' @details Overview: hogwash reads in one phenotype (either continuous or
+#'  binary), a matrix of binary genotypes, and a phylogenetic tree. Given these
+#'  inputs it performs an ancestral reconstruction of that phenotype and each
+#'  genotype. The ancestral reconstructions are used to perform one of several
+#'  tests to associate the the genotypes with the phenotype:
+#'  \enumerate{
+#'   \item Continuous Test
+#'   \item Synchronous Test
+#'   \item PhyC (Farhat et al.)
+#' }
+#'  Once a test finishes running it returns (i) p-values for all genotypes
+#'  tested, (ii) a manhattan plot of those p-values; if any of the genotypes
+#'  tested were significant associated with the phenotype after FDR correction
+#'  it also returns (iii) a list of significant hits and (iv) figures of the
+#'  genotype & phenotype reconstructions on the tree.
 #'
-#'   Grouping: A feature of hogwash is the ability to organize genotypes into
-#'   biologically meaningful groups. Testing for an association between an
-#'   individual SNP and a phenotype is quite stringent, but patterns may emerge
-#'   when grouping together biologically related genotypes. For example,
-#'   grouping together all variants (insertions, deletions and SNPs) within a
-#'   gene or promoter region could allow the user to identify a particular gene
-#'    as being associated with a phenotype while any individual variant within
-#'    that gene may not have deep penetrance in the isolates being tested.
-#'    Grouped genotypes could have increased power to identify convergent
-#'    evolution because they captures larger trends in functional impact at the
-#'    group level and reduce the multiple testing correction burden. Use cases
-#'    for this method could be to group SNPs into genes, kmers or genes into
-#'    pathways, etc... Each of the three tests can be run on disaggregated data
-#'     or aggregated data with the inclusion of a grouping key which is
-#'     described later on the inputs page.
+#'  Grouping: A feature of hogwash is the ability to organize genotypes into
+#'  biologically meaningful groups. Testing for an association between an
+#'  individual SNP and a phenotype is quite stringent, but patterns may emerge
+#'  when grouping together biologically related genotypes. For example,
+#'  grouping together all variants (insertions, deletions and SNPs) within a
+#'  gene or promoter region could allow the user to identify a particular gene
+#'  as being associated with a phenotype while any individual variant within
+#'  that gene may not have deep penetrance in the isolates being tested.
+#'  Grouped genotypes could have increased power to identify convergent
+#'  evolution because they captures larger trends in functional impact at the
+#'  group level and reduce the multiple testing correction burden. Use cases
+#'  for this method could be to group SNPs into genes, kmers or genes into
+#'  pathways, etc... Each of the three tests can be run on disaggregated data
+#'  or aggregated data with the inclusion of a grouping key which is
+#'  described later on the inputs page.
 #' @param pheno Matrix. Dimensions: nrow = number of samples, ncol = 1. Either
-#'   continuous or binary (0/1). Row.names() must match tree$tip.label.
+#'  continuous or binary (0/1). Row.names() must match tree$tip.label.
 #' @param geno Matrix. Dimensions: nrow = number of samples, ncol = number of
-#'   genotypes. Binary (0/1). Row.names() must match tree$tip.label.
+#'  genotypes. Binary (0/1). Row.names() must match tree$tip.label.
 #' @param tree Phylo object. If unrooted, will be rooted using
-#'   phytools::midpoint.root() method.
+#'  phytools::midpoint.root() method.
 #' @param file_name Character. Name of output files.
 #' @param dir Character. Path to output directory.
 #' @param perm Integer. Number of permutations to run.
 #' @param fdr Numeric. False discovery rate. Between 0 and 1.
 #' @param bootstrap Numeric. Confidence threshold for tree bootstrap values.
-#' @param group_genotype_key Matrix. Dimenions: nrow = number of unique genotypes, ncol = 2.
+#' @param group_genotype_key Matrix. Dimenions: nrow = number of unique
+#'  genotypes, ncol = 2.
 #'
 #' @export
 #' @examples
-#' # Continuous Test for continuous phenotype
-#' phenotype <- hogwash::growth
+#' \strong{Continuous Test for continuous phenotype}
+#' \code{phenotype <- hogwash::growth
 #' genotype <- hogwash::snp_genotype
 #' tree <- hogwash::tree
 #' hogwash(pheno = phenotype,
 #'         geno = genotype,
-#'         tree = tree)
+#'         tree = tree)}
 #'
-#' # Continuous Test while grouping SNPs into genes
-#' phenotype <- hogwash::growth
+#' \strong{Continuous Test while grouping SNPs into genes}
+#' \code{phenotype <- hogwash::growth
 #' genotype <- hogwash::snp_genotype
 #' tree <- hogwash::tree
 #' key <- hogwash::snp_gene_key
 #' hogwash(pheno = phenotype,
 #'         geno = genotype,
 #'         tree = tree,
-#'         group_genotype_key = key)
+#'         group_genotype_key = key)}
 #'
-#' # Both Synchronous Test & PhyC for discrete phenotype
-#' phenotype <- hogwash::antibiotic_resistance
+#' \strong{Both Synchronous Test & PhyC for discrete phenotype}
+#' \code{phenotype <- hogwash::antibiotic_resistance
 #' genotype <- hogwash::snp_genotype
 #' tree <- hogwash::tree
 #' hogwash(pheno = phenotype,
 #'         geno = genotype,
-#'         tree = tree)
+#'         tree = tree)}
 #'
-#' # Both Synchronous Test & PhyC while grouping SNPs into genes
-#' phenotype <- hogwash::antibiotic_resistance
+#' \strong{Both Synchronous Test & PhyC while grouping SNPs into genes}
+#' \code{phenotype <- hogwash::antibiotic_resistance
 #' genotype <- hogwash::snp_genotype
 #' tree <- hogwash::tree
 #' key <- hogwash::snp_gene_key
 #' hogwash(pheno = phenotype,
 #'         geno = genotype,
 #'         tree = tree,
-#'         group_genotype_key = key)
+#'         group_genotype_key = key)}
+#'
 #' @author Katie Saund
-#' @references Farhat MR, Shapiro BJ, Kieser KJ, et al. Genomic analysis identifies targets of convergent positive selection in drug-resistant Mycobacterium tuberculosis. Nat Genet. 2013;45(10):1183–1189. doi:10.1038/ng.2747
-
+#'
+#' @references Farhat MR, Shapiro BJ, Kieser KJ, et al. Genomic analysis
+#'  identifies targets of convergent positive selection in drug-resistant
+#'  \emph{Mycobacterium tuberculosis}. Nat Genet. 2013;45(10):1183–1189.
+#'  doi:10.1038/ng.2747
+#'
 hogwash <- function(pheno,
                     geno,
                     tree,
@@ -109,8 +117,13 @@ hogwash <- function(pheno,
   if (!is.null(group_genotype_key)) {
     args$group_genotype <- TRUE
   }
-  args$discrete_or_continuous <- check_input_format(args$phenotype, args$tree, args$genotype, args$output_name, args$output_dir, args$perm, args$fdr)
+  args$discrete_or_continuous <-
+    check_input_format(args$phenotype,
+                       args$tree,
+                       args$genotype,
+                       args$output_name,
+                       args$output_dir,
+                       args$perm,
+                       args$fdr)
   select_test_type(args)
 }
-
-# End of script ----------------------------------------------------------------
