de71653c5d6c7d0c3badcf37313cd60a08de9b39
katiembrennan <ktbrennan3@gmail.com>
2019-07-26 10:24:16 -0400

    Added roxygen2 documentation

--- R/select_test_type.R
+++ R/select_test_type.R
@@ -1,24 +1,21 @@
+#' select_test_type
+#'
+#' @description Given the type of input to the GWAS program, run either the
+#'  continuous test or both of the discrete tests (Synchronous and PhyC).
+#' @param args Object with all of the user provided inputs necessary to run the
+#'  gwas.
+#'
+#' @noRd
+#'
 select_test_type <- function(args){
-  # Function description -------------------------------------------------------
-  # Given the type of input to the GWAS program, run either the continuous test
-  # or both of the discrete tests.
-  #
-  # Inputs:
-  # args$discrete_or_continuous: Character string. Either "discrete" or
-  #                              "continuous".
-  #
-  # Output:
-  # Saved plots and an .Rdata file.
-  #
-  # Check inputs -------------------------------------------------------------
+  # Check inputs ---------------------------------------------------------------
   check_str_is_discrete_or_continuous(args$discrete_or_continuous)
 
-  # Function -----------------------------------------------------------------
+  # Function -------------------------------------------------------------------
   if (args$discrete_or_continuous == "continuous") {
     run_continuous(args)
   } else {
     run_binary_transition(args)
     run_binary_original(args)
   }
-
 } # end select_test_type()
