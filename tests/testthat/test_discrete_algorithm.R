b32dfeebe2e451482c837c1db847ef5e5de74857
katiembrennan <ktbrennan3@gmail.com>
2019-07-26 12:17:35 -0400

    limited lines to 80 characters

--- tests/testthat/test_discrete_algorithm.R
+++ tests/testthat/test_discrete_algorithm.R
@@ -1,7 +1,8 @@
 context("Discrete algorithm") #------------------------------------------------#
 
 # test calculate_permutation_based_p_value
-test_that("calculate_permutation_based_p_value returns a significant p-value when statistics are much lower than observed", {
+test_that("calculate_permutation_based_p_value returns a significant p-value
+          when statistics are much lower than observed", {
   nperm <- 1000
   permuted_tests <- rep(0.1, nperm)
   real_test <- 0.9
@@ -9,7 +10,8 @@ test_that("calculate_permutation_based_p_value returns a significant p-value whe
   expect_true(calculate_permutation_based_p_value(permuted_tests, real_test, nperm) < alpha)
 })
 
-test_that("calculate_permutation_based_p_value returns a non-significant p-value when statistics are cenetered around observed", {
+test_that("calculate_permutation_based_p_value returns a non-significant p-value
+          when statistics are cenetered around observed", {
   nperm <- 1000
   permuted_tests <- rnorm(n = nperm, mean = 0)
   real_test <- 0
@@ -18,7 +20,8 @@ test_that("calculate_permutation_based_p_value returns a non-significant p-value
 })
 
 # test count_hits_on_edges
-test_that("count_hits_on_edges returns 3 edges shared and 7 edges only with genotype given this test data", {
+test_that("count_hits_on_edges returns 3 edges shared and 7 edges only with
+          genotype given this test data", {
   num_samples <- 6
   set.seed(1)
   temp_tree <- ape::rtree(num_samples)
@@ -37,7 +40,8 @@ test_that("count_hits_on_edges returns 3 edges shared and 7 edges only with geno
   expect_equal(results$only_geno_present[1], num_just_geno_present)
 })
 
-test_that("count_hits_on_edges returns 0 edges shared and 0 edges only with genotype given this all absent genotype", {
+test_that("count_hits_on_edges returns 0 edges shared and 0 edges only with
+          genotype given this all absent genotype", {
   num_samples <- 6
   temp_tree <- ape::rtree(num_samples)
   temp_tree$node.labels <- rep(100, ape::Nnode(temp_tree))
@@ -53,7 +57,8 @@ test_that("count_hits_on_edges returns 0 edges shared and 0 edges only with geno
 })
 
 # test discrete_calculate_pvals
-test_that("discrete_calculate_pvals returns expected results given this dummy data", {
+test_that("discrete_calculate_pvals returns expected results given this dummy
+          data", {
   num_samples <- 6
   num_genotypes <- 15
   set.seed(1)
@@ -67,13 +72,29 @@ test_that("discrete_calculate_pvals returns expected results given this dummy da
   }
   temp_geno_trans[[15]] <- c(0, 0, 1, 1, 0, 0, 0, 1, 0, 0)
   temp_pheno_trans <- c(1, 1, 1, 1, 0, 0, 0, 0, 0, 0)
-  temp_geno <- matrix(1, ncol = num_genotypes, nrow = ape::Ntip(temp_tree)) # doesn't match recon or transition, just made up for now.
+
+  temp_geno <- matrix(1, ncol = num_genotypes, nrow = ape::Ntip(temp_tree))
+  # doesn't match recon or transition, just made up for now.
+
   temp_perm <- 8
   temp_fdr <- 0.25
 
-  expect_error(discrete_calculate_pvals(temp_geno_trans, temp_pheno_trans, temp_tree, temp_geno, temp_perm, temp_fdr, temp_hi_conf_edges), NA)
+  expect_error(discrete_calculate_pvals(temp_geno_trans,
+                                        temp_pheno_trans,
+                                        temp_tree,
+                                        temp_geno,
+                                        temp_perm,
+                                        temp_fdr,
+                                        temp_hi_conf_edges),
+               NA)
 
-  disc_trans_results <- discrete_calculate_pvals(temp_geno_trans, temp_pheno_trans, temp_tree, temp_geno, temp_perm, temp_fdr, temp_hi_conf_edges)
+  disc_trans_results <- discrete_calculate_pvals(temp_geno_trans,
+                                                 temp_pheno_trans,
+                                                 temp_tree,
+                                                 temp_geno,
+                                                 temp_perm,
+                                                 temp_fdr,
+                                                 temp_hi_conf_edges)
 
   expect_equal(round(as.numeric(disc_trans_results$hit_pvals[1]), 3), 0.444)
   expect_equal(disc_trans_results$observed_overlap[1], 1)
@@ -81,7 +102,8 @@ test_that("discrete_calculate_pvals returns expected results given this dummy da
 })
 
 # test discrete_permutation
-test_that("discrete_permutation returns expected results given this dummy data", {
+test_that("discrete_permutation returns expected results given this dummy
+          data", {
   num_samples <- 6
   num_genotypes <- 15
   set.seed(1)
@@ -95,17 +117,27 @@ test_that("discrete_permutation returns expected results given this dummy data",
   }
   temp_geno_trans[[15]] <- c(0, 0, 1, 1, 0, 0, 0, 1, 0, 0)
   temp_pheno_trans <- c(1, 1, 1, 1, 0, 0, 0, 0, 0, 0)
-  temp_geno <- matrix(1, ncol = num_genotypes, nrow = ape::Ntip(temp_tree)) # doesn't match recon or transition, just made up for now.
+
+  temp_geno <- matrix(1, ncol = num_genotypes, nrow = ape::Ntip(temp_tree))
+  # doesn't match recon or transition, just made up for now.
+
   temp_perm <- 8
 
   temp_num_edges_with_geno_trans <- sapply(temp_geno_trans, function(x) sum(x))
   temp_num_hi_conf_edges <- sapply(temp_hi_conf_edges, function(x) sum(x))
 
   for (m in 1:num_genotypes) {
-    permuted_geno <- discrete_permutation(temp_tree, temp_perm, temp_num_edges_with_geno_trans, temp_num_hi_conf_edges, num_edge, temp_hi_conf_edges, m)
+    permuted_geno <- discrete_permutation(temp_tree,
+                                          temp_perm,
+                                          temp_num_edges_with_geno_trans,
+                                          temp_num_hi_conf_edges,
+                                          num_edge,
+                                          temp_hi_conf_edges,
+                                          m)
     expect_equal(nrow(permuted_geno), temp_perm)
     expect_equal(ncol(permuted_geno), num_edge)
-    expect_lt(max(rowSums(permuted_geno)), temp_num_edges_with_geno_trans[m] + 1)
+    expect_lt(max(rowSums(permuted_geno)),
+              temp_num_edges_with_geno_trans[m] + 1)
   }
 
   edges_selected <- NULL
@@ -114,13 +146,22 @@ test_that("discrete_permutation returns expected results given this dummy data",
     edges_selected <- c(edges_selected, temp)
   }
 
-  edge_probability <- temp_tree$edge.length / sum(temp_tree$edge.length) # high confidence is one for all genotypes, so can do just one calculation for all genotypes in this case.
-  edges_selected_other_way <- sample(1:num_edge, size = length(edges_selected), replace = TRUE, prob = edge_probability)
-  ks_results <-  withCallingHandlers(stats::ks.test(edges_selected_other_way, edges_selected),
-                                     warning = function(w) {
-                                       if (grepl("cannot compute exact p-value with ties", w$message))
-                                         invokeRestart("muffleWarning")
-                                     } )
+  edge_probability <- temp_tree$edge.length / sum(temp_tree$edge.length)
+  # high confidence is one for all genotypes, so can do just one calculation for
+  #  all genotypes in this case.
+
+  edges_selected_other_way <- sample(1:num_edge,
+                                     size = length(edges_selected),
+                                     replace = TRUE,
+                                     prob = edge_probability)
+  ks_results <-
+    withCallingHandlers(stats::ks.test(edges_selected_other_way,
+                                       edges_selected),
+                        warning = function(w) {
+                          if (grepl("cannot compute exact p-value with ties",
+                                    w$message))
+                            invokeRestart("muffleWarning")
+                          } )
   expect_true(ks_results$p.value > 0.05)
 })
 
@@ -132,6 +173,14 @@ test_that("count_empirical_both_present gives X given Y", {
   temp_hi_conf_edge[[2]] <- c(0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1)
   temp_permuted_mat <- matrix(stats::rbinom(120, 1, .5), ncol = 12, nrow = 10)
   temp_index <- 1
-  expect_error(count_empirical_both_present(temp_permuted_mat, temp_pheno_vec, temp_hi_conf_edge, temp_index), NA)
-  expect_warning(count_empirical_both_present(temp_permuted_mat, temp_pheno_vec, temp_hi_conf_edge, temp_index), NA)
+  expect_error(count_empirical_both_present(temp_permuted_mat,
+                                            temp_pheno_vec,
+                                            temp_hi_conf_edge,
+                                            temp_index),
+               NA)
+  expect_warning(count_empirical_both_present(temp_permuted_mat,
+                                              temp_pheno_vec,
+                                              temp_hi_conf_edge,
+                                              temp_index),
+                 NA)
 })
