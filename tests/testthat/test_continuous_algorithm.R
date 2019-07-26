abe2f024055f2be64981639107bc5e3882318238
katiembrennan <ktbrennan3@gmail.com>
2019-07-26 12:15:02 -0400

    limited lines to 80 characters

--- tests/testthat/test_continuous_algorithm.R
+++ tests/testthat/test_continuous_algorithm.R
@@ -59,22 +59,33 @@ test_that("calc_sig gives expected results given valid inputs", {
     temp_geno_recon[[i]] <- rep(1, ape::Nedge(temp_tree))
   }
   set.seed(1)
-  temp_pheno <- matrix(rnorm(ape::Nedge(temp_tree) * 2), ncol = 2, nrow = ape::Nedge(temp_tree))
-  results <- calc_sig(temp_geno, temp_perm, temp_geno_trans, temp_tree, temp_pheno, temp_conf, temp_geno_recon)
+  temp_pheno <- matrix(rnorm(ape::Nedge(temp_tree) * 2),
+                       ncol = 2,
+                       nrow = ape::Nedge(temp_tree))
+  results <- calc_sig(temp_geno,
+                      temp_perm,
+                      temp_geno_trans,
+                      temp_tree,
+                      temp_pheno,
+                      temp_conf,
+                      temp_geno_recon)
   expect_equal(results$num_genotypes, num_loci)
   expect_equal(round(results$observed_ks_stat[1], 3), 0.333)
   expect_equal(round(results$all_edges_median[1], 3), 0.993)
   expect_equal(round(results$trans_median[1], 2), 1.2)
-  expect_equal(results$observed_pheno_non_trans_delta[[1]], c(0.4890317, 1.3942315, 0.7832583))
-  expect_equal(round(results$observed_pheno_trans_delta[[1]], 3), c(1.202, 2.347, 0.638))
-  expect_equal(round(results$ks_statistics[[1]][1:5], 3), c(0.50, 0.25, 0.75, 0.80, 0.50))
+  expect_equal(results$observed_pheno_non_trans_delta[[1]],
+               c(0.4890317, 1.3942315, 0.7832583))
+  expect_equal(round(results$observed_pheno_trans_delta[[1]], 3),
+               c(1.202, 2.347, 0.638))
+  expect_equal(round(results$ks_statistics[[1]][1:5], 3),
+               c(0.50, 0.25, 0.75, 0.80, 0.50))
   expect_equal(round(results$pvals, 3), rep(0.98, num_loci))
 })
 
 
-test_that("calc_sig returns a significant p-value when
-          phenotype change is noticeably higher on transition edges than on
-          non-transition tree edges", {
+test_that("calc_sig returns a significant p-value when phenotype change is
+           noticeably higher on transition edges than on non-transition tree
+          edges", {
   num_isolates <- 5
   num_loci <- 2
   set.seed(1)
@@ -90,22 +101,36 @@ test_that("calc_sig returns a significant p-value when
     temp_geno_recon[[i]]            <- c(1, 1, 1, 1, 1, 1, 1, 1)
   }
   set.seed(1)
-  temp_pheno <- matrix(c(-1, -1, 10, -.5, 10, 10, 0, 0, 0, -.5, 11, 30, 100, 40, 150, 0.1), ncol = 2, nrow = ape::Nedge(temp_tree))
+  temp_pheno <-
+    matrix(c(-1, -1, 10, -.5, 10, 10, 0, 0, 0, -.5, 11, 30, 100, 40, 150, 0.1),
+           ncol = 2,
+           nrow = ape::Nedge(temp_tree))
   # Transition edges deltas: 10, 90, 30
   # Non-transition edge deltas: 3, 1, 1
-  results <- calc_sig(temp_geno, temp_perm, temp_geno_trans, temp_tree, temp_pheno, temp_conf, temp_geno_recon)
+  results <- calc_sig(temp_geno,
+                      temp_perm,
+                      temp_geno_trans,
+                      temp_tree,
+                      temp_pheno,
+                      temp_conf,
+                      temp_geno_recon)
   alpha <- 0.06
   expect_true(results$pvals[1] < alpha)
-  expect_equal(results$all_edges_median[[1]], median(abs(temp_pheno[ , 1] - temp_pheno[ , 2])))
-  expect_equal(results$trans_median[[1]], median(abs(temp_pheno[4:7 , 1] - temp_pheno[4:7 , 2])))
+  expect_equal(results$all_edges_median[[1]],
+               median(abs(temp_pheno[ , 1] - temp_pheno[ , 2])))
+  expect_equal(results$trans_median[[1]],
+               median(abs(temp_pheno[4:7 , 1] - temp_pheno[4:7 , 2])))
   expect_equal(round(results$observed_ks_stat[1], 2), 1)
-  expect_equal(results$observed_pheno_non_trans_delta[[1]], abs(temp_pheno[c(1:3,8), 1] - temp_pheno[c(1:3,8), 2]))
-  expect_equal(round(results$observed_pheno_trans_delta[[1]], 3), abs(temp_pheno[4:7, 1] - temp_pheno[4:7, 2]))
-  expect_equal(round(results$ks_statistics[[1]][1:5], 3), c(0.40, 0.50, 1.00, 0.25, 0.25))
+  expect_equal(results$observed_pheno_non_trans_delta[[1]],
+               abs(temp_pheno[c(1:3,8), 1] - temp_pheno[c(1:3,8), 2]))
+  expect_equal(round(results$observed_pheno_trans_delta[[1]], 3),
+               abs(temp_pheno[4:7, 1] - temp_pheno[4:7, 2]))
+  expect_equal(round(results$ks_statistics[[1]][1:5], 3),
+               c(0.40, 0.50, 1.00, 0.25, 0.25))
 })
 
-test_that("calc_sig returns a non-significant p-value when
-          phenotype change is identical on transition and non-transition edges", {
+test_that("calc_sig returns a non-significant p-value when phenotype change is
+          identical on transition and non-transition edges", {
   num_isolates <- 4
   num_loci <- 2
   set.seed(1)
@@ -121,22 +146,33 @@ test_that("calc_sig returns a non-significant p-value when
     temp_geno_recon[[i]]            <- c(1, 1, 1, 1, 1, 1)
   }
   set.seed(1)
-  temp_pheno <- matrix(c(-15, -15, 10, 0, 10, 10, -10, 0, 7, 5, 25, 7), ncol = 2, nrow = ape::Nedge(temp_tree))
+  temp_pheno <- matrix(c(-15, -15, 10, 0, 10, 10, -10, 0, 7, 5, 25, 7),
+                       ncol = 2,
+                       nrow = ape::Nedge(temp_tree))
   # Transition edges deltas: 5 15 3
   # Non-transition edge deltas: 5 15 3
-  results <- calc_sig(temp_geno, temp_perm, temp_geno_trans, temp_tree, temp_pheno, temp_conf, temp_geno_recon)
+  results <- calc_sig(temp_geno,
+                      temp_perm,
+                      temp_geno_trans,
+                      temp_tree,
+                      temp_pheno,
+                      temp_conf,
+                      temp_geno_recon)
   alpha <- 0.01
   expect_true(results$pvals[1] > alpha)
-  expect_equal(results$all_edges_median[[1]], median(abs(temp_pheno[ , 1] - temp_pheno[ , 2])))
-  expect_equal(results$trans_median[[1]], median(abs(temp_pheno[4:6 , 1] - temp_pheno[4:6 , 2])))
+  expect_equal(results$all_edges_median[[1]],
+               median(abs(temp_pheno[ , 1] - temp_pheno[ , 2])))
+  expect_equal(results$trans_median[[1]],
+               median(abs(temp_pheno[4:6 , 1] - temp_pheno[4:6 , 2])))
   expect_equal(round(results$observed_ks_stat[1], 2), 0)
-  expect_equal(results$observed_pheno_non_trans_delta[[1]], abs(temp_pheno[1:3 , 1] - temp_pheno[1:3 , 2]))
-  expect_equal(round(results$observed_pheno_trans_delta[[1]], 3), abs(temp_pheno[4:6 , 1] - temp_pheno[4:6 , 2]))
+  expect_equal(results$observed_pheno_non_trans_delta[[1]],
+               abs(temp_pheno[1:3 , 1] - temp_pheno[1:3 , 2]))
+  expect_equal(round(results$observed_pheno_trans_delta[[1]], 3),
+               abs(temp_pheno[4:6 , 1] - temp_pheno[4:6 , 2]))
 })
 
-test_that("calc_sig returns an error when the only
-          confident edges are transition edges. (All non-transition edges are
-          low confidence)", {
+test_that("calc_sig returns an error when the only confident edges are
+          transition edges. (All non-transition edges are low confidence)", {
   num_isolates <- 4
   num_loci <- 2
   set.seed(1)
@@ -152,16 +188,25 @@ test_that("calc_sig returns an error when the only
     temp_geno_recon[[i]]            <- c(1, 1, 1, 1, 1, 1)
   }
   set.seed(1)
-  temp_pheno <- matrix(c(-15, -15, 10, 0, 10, 10, -10, 0, 7, 5, 25, 7), ncol = 2, nrow = ape::Nedge(temp_tree))
+  temp_pheno <- matrix(c(-15, -15, 10, 0, 10, 10, -10, 0, 7, 5, 25, 7),
+                       ncol = 2,
+                       nrow = ape::Nedge(temp_tree))
   # Transition edges deltas: 5 15 3
   # Non-transition edge deltas: 5 15 3
-  expect_error(calc_sig(temp_geno, temp_perm, temp_geno_trans, temp_tree, temp_pheno, temp_conf, temp_geno_recon))
+  expect_error(calc_sig(temp_geno,
+                        temp_perm,
+                        temp_geno_trans,
+                        temp_tree,
+                        temp_pheno,
+                        temp_conf,
+                        temp_geno_recon))
 })
 
 
 
 # test get_hi_conf_tran_indices
-test_that("get_hi_conf_tran_indices returns only high confidence transition edges given this test data", {
+test_that("get_hi_conf_tran_indices returns only high confidence transition
+          edges given this test data", {
   num_isolates <- 5
   num_loci <- 8
   set.seed(1)
@@ -174,14 +219,16 @@ test_that("get_hi_conf_tran_indices returns only high confidence transition edge
     temp_conf[[i]] <- c(1, 1, 1, 0, 0, 1, 1, 1)
   }
   index <- 1
-  indices <- get_hi_conf_tran_indices(temp_geno_trans, temp_conf, index, temp_tree)
+  indices <-
+    get_hi_conf_tran_indices(temp_geno_trans, temp_conf, index, temp_tree)
   expect_equal(indices$trans_index, c(1, 3, 7))
   expect_equal(indices$non_trans_index, c(2, 6, 8))
 })
 
 
 # test continuous_permutation
-test_that("continuous_permutation is gives consistent results with this test set", {
+test_that("continuous_permutation is gives consistent results with this test
+          set", {
   num_isolates <- 40
   num_loci <- 80
   set.seed(1)
@@ -200,23 +247,30 @@ test_that("continuous_permutation is gives consistent results with this test set
   expect_equal(results$hi_conf_edges, odd_numbers)
   expect_equal(nrow(results$permuted_trans_index_mat), perm)
   expect_equal(ncol(results$permuted_trans_index_mat), length(index$trans_index))
-  expect_equal(results$permuted_trans_index_mat[1, 1:10], c(18, 8, 19, 5, 23, 25, 20, 21, 15, 38))
+  expect_equal(results$permuted_trans_index_mat[1, 1:10],
+               c(18, 8, 19, 5, 23, 25, 20, 21, 15, 38))
 })
 
 # test calculate_empirical_pheno_delta
-test_that("calculate_empirical_pheno_delta returns a list of valid ks-test results of length perm", {
+test_that("calculate_empirical_pheno_delta returns a list of valid ks-test
+          results of length perm", {
   perm <- 1000
   num_isolates <- 40
   nedge <- 78
   set.seed(1)
-  temp_permuted_mat <- matrix(round(rnorm(perm * num_isolates, mean = 30, sd = 5), 0), nrow = perm, ncol = num_isolates)
+  temp_permuted_mat <-
+    matrix(round(rnorm(perm * num_isolates, mean = 30, sd = 5), 0),
+           nrow = perm,
+           ncol = num_isolates)
   set.seed(1)
   temp_pheno <- matrix(rnorm(2 * nedge), nrow = nedge, ncol = 2)
   temp_conf <- seq(from = 1, to = max(temp_permuted_mat), by = 2)
-  results <- calculate_empirical_pheno_delta(perm, temp_permuted_mat, temp_conf, temp_pheno)
+  results <- calculate_empirical_pheno_delta(perm,
+                                             temp_permuted_mat,
+                                             temp_conf,
+                                             temp_pheno)
   length(results)
   expect_equal(length(results), perm)
   expect_true(max(results) < 1)
   expect_true(min(results) > 0)
 })
-
