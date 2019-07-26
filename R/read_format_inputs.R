665f60783a16f10d6f5ed689f1735ec307f75abb
katiembrennan <ktbrennan3@gmail.com>
2019-07-26 10:12:59 -0400

    Updated documentation to roxygen2 style

--- R/read_format_inputs.R
+++ R/read_format_inputs.R
@@ -1,24 +1,29 @@
+#' check_input_format
+#'
+#' @description Check that all of the inputs into hogwash are in the valid
+#'  format.
+#' @param pheno  Matrix. Phenotype. Phenotype. matrix should have same rows as
+#'  tr$tip.label and exactly 1 column. Phenotype rownames should be in the same
+#'  order as the tr$tip.label.
+#' @param tr Phylo. Tree.
+#' @param geno Matrix. Genotype. Genotype matrix should have same rows as
+#' tr$tip.label, at least 2 genotypes in the columns. Genotype rownames should
+#'  be in the same order as the tr$tip.label.
+#' @param name Character. Output name.
+#' @param dir  Character. Output path.
+#' @param perm  Number. Times to shuffle the data on the tree to create a null
+#'  distribution for the permutation test.
+#' @param fdr Number. False discovery rate. Between 0 and 1.
+#'
+#' @return discrete_or_continuous. Character. Either "discrete" or "continuous".
+#'  Describes the input phenotype.
+#' @noRd
 check_input_format <- function(pheno, tr, geno, name, dir, perm, fdr){
-  # Function description -------------------------------------------------------
-  # Check that all of the inputs into phyC are in the valid format.
-  #
-  # Input:
-  # pheno. Matrix. Phenotype.
-  # tr.    Phylo. Tree.
-  # geno.  Matrix. Genotype.
-  # name.  Character. Output name.
-  # dir.   Character. Output path.
-  # perm.  Number. Times to shuffle the data on the tree to create a null distribution for the permutation test.
-  # fdr.   Number. False discovery rate. Between 0 and 1.
-  #
-  # Output:
-  # discrete_or_continuous. Character. Either "discrete" or "continuous". Describes the input phenotype.
-  #
   # Check input ----------------------------------------------------------------
-  check_dimensions(geno, ape::Ntip(tr), 2, NULL, 2) # Genotype matrix should have same rows as tr$tip.label, at least 2 genotypes in the columns
-  check_dimensions(pheno, ape::Ntip(tr), 2, 1, 1) # Phnoetype matrix should have same rows as tr$tip.label and exactly 1 column
-  check_rownames(geno, tr) # Genotype rownames should be in the same order as the tr$tip.label
-  check_rownames(pheno, tr) # Phenotype rownames should be in the same order as the tr$tip.label
+  check_dimensions(geno, ape::Ntip(tr), 2, NULL, 2)
+  check_dimensions(pheno, ape::Ntip(tr), 2, 1, 1)
+  check_rownames(geno, tr)
+  check_rownames(pheno, tr)
   check_for_NA_and_inf(geno)
   check_for_NA_and_inf(pheno)
   check_if_binary_matrix(geno)
