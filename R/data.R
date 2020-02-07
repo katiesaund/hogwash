7cdd55c1ae28baa6c28321b6f4aed6b12ba4d83f
Katie Saund <katiephd@umich.edu>
2020-02-07 15:45:58 -0500

    Reflow comments

--- R/data.R
+++ R/data.R
@@ -1,16 +1,19 @@
 #' Binary phenotype
 #'
 #' An example binary phenotype matrix.
-#' @format A matrix with 7 rows and 1 columns. Each row name corresponds to a tree tip.
+#' @format A matrix with 7 rows and 1 columns. Each row name corresponds to a
+#'   tree tip.
 #' \describe{
-#'   \item{resistance}{Antibiotic resistance encoded as 1; antibiotic susceptible encoded as 0}
+#'   \item{resistance}{Antibiotic resistance encoded as 1; antibiotic
+#'   susceptible encoded as 0}
 #'  }
 "antibiotic_resistance"
 
 #' Continuous phenotype
 #'
 #' An example continous phenotype matrix.
-#' @format A matrix with 7 rows and 1 columns. Each row name corresponds to a tree tip.
+#' @format A matrix with 7 rows and 1 columns. Each row name corresponds to a
+#'   tree tip.
 #' \describe{
 #'   \item{growth}{Dummy growth rates}
 #'  }
@@ -29,7 +32,8 @@
 #' Binary genotype
 #'
 #' An example binary genotype matrix.
-#' @format A matrix with 7 rows and 8 columns. Rows correspond to isolates and tree tips. Columns correspond to genotypes.
+#' @format A matrix with 7 rows and 8 columns. Rows correspond to isolates and
+#'   tree tips. Columns correspond to genotypes.
 #' \describe{
 #'   \item{SNP1}{SNP1 present encoded as 1, absent encoded as 0}
 #'  }
