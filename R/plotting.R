675f1705e8bc18c79c05ad980c177d16516cddf3
Katie Saund <katiephd@umich.edu>
2020-04-09 10:19:40 -0400

    Update print statements

--- R/plotting.R
+++ R/plotting.R
@@ -170,12 +170,10 @@ hist_abs_hi_conf_delta_pheno <- function(all_trans,
                                          trans_color){
   # Check inputs ---------------------------------------------------------------
   check_is_number(index)
-  print("index")
+  print("inside function index")
   print(index)
-  print("all_trans")
-  print(all_trans)
-  print("length(all_trans)")
-  print(length(all_trans))
+  print("all_trans$num_genotypes")
+  print(all_trans$num_genotypes)
   if (index > all_trans$num_genotypes) {
     stop("Index must correspond to one of the genotypes")
   }
@@ -1277,14 +1275,14 @@ plot_continuous_results <- function(disc_cont,
       # Histogram of the |delta phenotype| for only high conf. edges, colored by
       #   genotype non-transition vs transition edges
 
-      print("j")
+      print("j outside function")
       print(j)
       print("pval_all_transition$hit_pvals[j, ]")
       print(pval_all_transition$hit_pvals[j, ])
-      print("results_all_trans")
-      print(results_all_trans)
-      print("length(results_all_trans)")
-      print(length(results_all_trans))
+      print("str(results_all_trans)")
+      print(str(results_all_trans))
+      print("results_all_trans$num_genotypes")
+      print(results_all_trans$num_genotypes)
 
       hist_abs_hi_conf_delta_pheno(results_all_trans,
                                    tr,
