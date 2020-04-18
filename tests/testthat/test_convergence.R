99f343ce8d763bb373cdde904fccd9ba1a28d3fd
Katie Saund <katiephd@umich.edu>
2020-04-17 20:40:33 -0400

    Rename gamma/epsilon files to convergence

--- tests/testthat/test_convergence.R
+++ tests/testthat/test_convergence.R
@@ -1,4 +1,4 @@
-context("Epsilon") #-----------------------------------------------------------#
+context("Convergence") #---------------------------------------------------#
 test_that("calculate_continuous_gamma gives expected results for valid continuous input", {
   # Set up
   set.seed(1)
